2b555685a849ad225a843b2794010b75e9766dfd
rdm-barreto <rdm.barreto@gmail.com>
2024-12-11 17:09:17 -0300

    Tests

--- backend/src/products/products.controller.spec.ts
+++ backend/src/products/products.controller.spec.ts
@@ -1,20 +1,46 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/sequelize';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { Product } from './entities/product.entity';
+import { CreateProductDto } from './dto/create-product.dto';
+
+const product = {
+  name: 'Product1',
+  price: 10,
+  category: 'C1',
+  rating: 1,
+};
+
+const createProductDto: CreateProductDto = product;
 
 describe('ProductsController', () => {
-  let controller: ProductsController;
+  let productsService: ProductsService;
+  let productsController: ProductsController;
+  let product: Product;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getModelToken(Product),
+          useValue: {},
+        },
+      ],
     }).compile();
 
-    controller = module.get<ProductsController>(ProductsController);
+    productsService = moduleRef.get<ProductsService>(ProductsService);
+    productsController = moduleRef.get<ProductsController>(ProductsController);
+    product = moduleRef.get<Product>(getModelToken(Product));
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a product', async () => {
+    const mockedResponseData = product;
+    jest
+      .spyOn(productsService, 'create')
+      .mockImplementation(() => Promise.resolve(mockedResponseData));
+    expect(await productsController.create(createProductDto)).toBe(mockedResponseData);
   });
 });
